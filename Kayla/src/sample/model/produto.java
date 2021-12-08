a7096b8b738766b3aeeae206c5ec2e5d7b96de51
kaylaZaj <95691608+kaylaZaj@users.noreply.github.com>
2021-12-08 11:45:22 -0300

    Add files via upload

---
+++ Kayla/src/sample/model/produto.java
@@ -0,0 +1,29 @@
+package sample.model;
+
+public class produto {
+
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String toString(){
+        return "Produto: " + nome
+                + ", Preço: " + preco;
+    }
+
+}
